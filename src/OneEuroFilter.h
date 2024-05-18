988d9de04888a1fcc3ca48005c089e29a6e3f8c1
Matthijs Hollemans <mail@hollance.com>
2024-05-18 16:23:41 +0200

    clean up filter a bit

--- src/OneEuroFilter.h
+++ src/OneEuroFilter.h
@@ -18,23 +18,25 @@ public:
         dy = 0.0;
     }
 
-    void setKrunch(double newKrunch) noexcept
+    void setKrunch(double krunch) noexcept
     {
-        krunch = newKrunch;
+        const double kk = krunch * krunch;
+        double skewed = kk * kk;
+        beta = 1.0 + 20000.0 * skewed;
     }
 
     double operator()(double x) noexcept
     {
-        double dx = (x - z) * 44100.0;
-        dy += dalpha * (dx - dy);
+        // Calculate derivative
+        double dx = x - z;
 
-// TODO: get dy working on a different scale so we don't need 44100 + 20000 factors
+        // Smoothen derivative very slowly
+        dy += dalpha * (dx * 40000.0 - dy);
 
-        const double kk = krunch * krunch;
-        double skewed = kk * kk;
-        double beta = 1.0 + 20000.0 * skewed;
+        // Calculate adaptive cutoff frequency
         double cutoff = 1.0 + beta * std::abs(dy);
 
+        // Filter the input signal using the new cutoff
         double alpha = smoothingFactor(cutoff);
         y += alpha * (x - y);
         z += alpha * (y - z);
@@ -52,14 +54,14 @@ private:
         // This is not the typical formula for calculating the coefficient
         // of a one-pole lowpass filter. It comes from the 1€ Filter paper
         // and allows the cutoff to go over Nyquist.
-        double r = 2.0 * M_PI * cutoff;
+        double r = 6.28318530717958647692528676655900577 * cutoff;
         return r / (r + sampleRate);
     }
 
     double sampleRate = 44100.0;
-    double krunch = 0.0;
-    double dalpha = 0.0;
-    double y = 0.0;
-    double z = 0.0;
-    double dy = 0.0;
+    double beta = 0.0;    // krunch amount
+    double dalpha = 0.0;  // filter coefficient for derivative
+    double y = 0.0;       // first filter
+    double z = 0.0;       // second filter
+    double dy = 0.0;      // filtered derivative
 };
